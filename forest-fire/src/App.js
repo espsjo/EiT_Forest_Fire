a30e0ff0074140f2061af8390aafe6b5531bfd70
espeasj <espeasj@stud.ntnu.no>
2024-02-14 10:15:35 +0100

    react app

---
+++ forest-fire/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={require("./logo.jpg")} className="App-logo" alt="fire"/>
+        <p>
+          Forest Fire Forecast
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
