58d273a881f42b9eeeb01971e2c3416314091373
jorgehl <jorgen.h.lund@gmail.com>
2024-02-21 15:02:14 +0100

    Added warning circles component.

--- forest-fire/src/App.jsx
+++ forest-fire/src/App.jsx
@@ -1,24 +1,25 @@
 import "./App.css";
-import axios from "axios";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import WarningCircle from "./components/WarningCircle.tsx";
+
+
 function App() {
-  axios
-    .get("https://localhost:7044/ReadingItems/")
-    .then((res) => console.log(res.data))
-    .catch((e) => console.log(e));
   return (
     <div className="App" style={{display: "flex", flexDirection: "column", alignContent: "center", alignItems: "center"}}>
       <header className="App-header" style={{ display: "flex", flexDirection: "row", width: "100%" }}>
         <p>Forest Fire Forecast</p>
         <img src={require("./logo.jpg")} className="App-logo" alt="fire" style={{marginLeft: "15px"}}/>
       </header>
+      <Testy />
       <div style={{height: "100vh", width: "80%"}}>
-        <MapContainer center={[63.446827, 10.421906]} zoom={13}>
+        <MapContainer center={[63.446827, 10.421906]} zoom={13} radius={20}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          <WarningCircle color={"yellow"} lat={"62.398519442261495"} long={"10.666932529755468"} radius={100}/>
+          <WarningCircle color={"red"} lat={"63.398519442261495"} long={"10.666932529758468"} radius={100}/>
         </MapContainer>
       </div>
     </div>
---
+++ forest-fire/src/components/WarningCircle.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { CircleMarker} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import React from "react";
+
+interface WarningCircleProps {
+    color: string,
+    lat: number,
+    long: number,
+    radius: number,
+}
+const WarningCircle = (props: WarningCircleProps) => {
+    return (
+        <CircleMarker
+        center={[props.lat, props.long]}
+        radius={props.radius}
+        pathOptions={{ fillColor: props.color, color: props.color, fillOpacity: 0.7 }}
+      />
+    )
+}
+
+export default WarningCircle;
